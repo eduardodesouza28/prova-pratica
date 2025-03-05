d408280fb7de3fd92be3f510c1ff90f25c67b3da
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-03-05 15:10:55 -0300

    criação do obstaculo em forma de array (WIP)

--- provaPratica.js
+++ provaPratica.js
@@ -29,7 +29,7 @@ class Racket extends Entity {
 
 class Ball extends Entity {
     constructor() {
-        super(60, 10, canvas.width - 340, canvas.height - 30)
+        super(60, 10, canvas.width - 308, canvas.height - 40)
     }
     draw() {
         ctx.fillStyle = 'white'
@@ -39,17 +39,32 @@ class Ball extends Entity {
     }
 }
 
+class Obstaule extends Entity {
+    constructor(posx) {
+        super(60, 10, posx = canvas.width - 340, canvas.height - 380)
+    }
+    draw() {
+        ctx.fillStyle = 'white'
+        ctx.fillRect(this.posx, this.posy, this.sizex, this.sizey)
+    }
+}
+
 class Game {
     constructor() {
+        this.obstacles = [this.obstacle = new Obstaule(), this.obstacle = new Obstaule(400)]
+        // for (i = 0; i <= 5; i++){
+        //     this.obstacles.push(this.obstacle = new Obstaule())
+        // }
         this.racket = new Racket()
         this.ball = new Ball()
         this.init()
     }
 
     init() {
-
         document.addEventListener("keypress", (e) => {
-
+            if (e.code === "ArrowRight") {
+                this.racket.posx += 10
+            }
         })
         this.loop()
     }
@@ -58,6 +73,9 @@ class Game {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         this.racket.draw()
         this.ball.draw()
+        this.obstacles.forEach(obs => {
+            obs.draw()
+        });
         requestAnimationFrame(() => this.loop())
     }
 }
